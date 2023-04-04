155d0c3521a45d7136b30a16ab3c76e98056cd98
Muhamed Mehmedovic <muhamedmehmedovic5@gmail.com>
2023-04-04 05:01:37 +0200

    changes

--- js/links.js
+++ js/links.js
@@ -9,3 +9,9 @@ $(document).ready(function () {
     }
   });
 });
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
