7b7ed564d3b51ab6fe825d9e61a29a70d3af309c
Muhamed Mehmedovic <muhamedmehmedovic5@gmail.com>
2023-04-05 01:50:31 +0200

    header & footer

--- dist/links.js
+++ dist/links.js
@@ -8,4 +8,32 @@ $(document).ready(function () {
       $(this).addClass("active");
     }
   });
+
+  const servicesToggle = document.getElementById("services-link");
+  const servicesDiv = document.getElementById("services-div");
+  servicesDiv.style.display = "none";
+  servicesToggle.onclick = function () {
+    if (servicesDiv.style.display !== "none") {
+      servicesDiv.style.display = "none";
+    } else {
+      servicesDiv.style.display = "block";
+    }
+  };
+
+  const resourcesToggle = document.getElementById("resources-link");
+  const resourcesDiv = document.getElementById("resources-div");
+  resourcesDiv.style.display = "none";
+  resourcesToggle.onclick = function () {
+    if (resourcesDiv.style.display !== "none") {
+      resourcesDiv.style.display = "none";
+    } else {
+      resourcesDiv.style.display = "block";
+    }
+  };
 });
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
--- js/links.js
+++ js/links.js
@@ -8,6 +8,28 @@ $(document).ready(function () {
       $(this).addClass("active");
     }
   });
+
+  const servicesToggle = document.getElementById("services-link");
+  const servicesDiv = document.getElementById("services-div");
+  servicesDiv.style.display = "none";
+  servicesToggle.onclick = function () {
+    if (servicesDiv.style.display !== "none") {
+      servicesDiv.style.display = "none";
+    } else {
+      servicesDiv.style.display = "block";
+    }
+  };
+
+  const resourcesToggle = document.getElementById("resources-link");
+  const resourcesDiv = document.getElementById("resources-div");
+  resourcesDiv.style.display = "none";
+  resourcesToggle.onclick = function () {
+    if (resourcesDiv.style.display !== "none") {
+      resourcesDiv.style.display = "none";
+    } else {
+      resourcesDiv.style.display = "block";
+    }
+  };
 });
 
 // When the user clicks on the button, scroll to the top of the document
