3f674c06a862fd03783ff91de415ed7fd2890cd3
Muhamed Mehmedovic <54117777+Muhamed112@users.noreply.github.com>
2023-04-09 18:34:24 +0200

    resource links fix

--- js/links.js
+++ js/links.js
@@ -9,32 +9,39 @@ $(document).ready(function () {
     }
   });
 
-  const servicesToggle = document.getElementById("services-link");
-  const servicesDiv = document.getElementById("services-div");
-  servicesDiv.style.display = "none";
-  servicesToggle.onclick = function () {
-    if (servicesDiv.style.display !== "none") {
-      servicesDiv.style.display = "none";
-    } else {
-      servicesDiv.style.display = "block";
-    }
-  };
-
-  const resourcesToggle = document.getElementById("resources-link");
-  const resourcesDiv = document.getElementById("resources-div");
-  resourcesDiv.style.display = "none";
-  resourcesToggle.onclick = function () {
-    if (resourcesDiv.style.display !== "none") {
-      resourcesDiv.style.display = "none";
-    } else {
-      resourcesDiv.style.display = "block";
-    }
-  };
+  $(".tab-link").click(function (e) {
+    e.preventDefault();
+    var pattern = /#.+/gi; //use regex to get anchor(==selector)
+    var contentID = e.target.toString().match(pattern)[0]; //get anchor
+    console.log(contentID);
+    $('.nav-tabs li a[data-bs-target="' + contentID + '"]').tab("show");
+  });
 });
+
+const servicesToggle = document.getElementById("services-link");
+const servicesDiv = document.getElementById("services-div");
+servicesDiv.style.display = "none";
+servicesToggle.onclick = function () {
+  if (servicesDiv.style.display !== "none") {
+    servicesDiv.style.display = "none";
+  } else {
+    servicesDiv.style.display = "block";
+  }
+};
+
+const resourcesToggle = document.getElementById("resources-link");
+const resourcesDiv = document.getElementById("resources-div");
+resourcesDiv.style.display = "none";
+resourcesToggle.onclick = function () {
+  if (resourcesDiv.style.display !== "none") {
+    resourcesDiv.style.display = "none";
+  } else {
+    resourcesDiv.style.display = "block";
+  }
+};
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
-
